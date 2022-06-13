56ff481f4e21896e31d698876901f7d2e02a969c
MR-KING-1 <107402675+MR-KING-1@users.noreply.github.com>
2022-06-13 15:16:35 +0500

    Create Fuck.py

---
+++ Fuck.py
@@ -0,0 +1,14 @@
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m KASE KA SATH VII SHARE NHI KRNA TOOL ! \033[1;37m")
+    from KING import Subscription
+    Subscription()
+elif bit == '32bit':
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
